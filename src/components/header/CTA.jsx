7213bc2711f8415bebd45201c015c3ccdfe9e988
Hinal Panchal <hpanchal@zmags.com>
2023-06-26 02:39:24 +0530

    Updating Resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Resume from '../../assets/Resume.pdf'
+import Hinal_Resume from '../../assets/Hinal_Resume.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={Resume} download className='btn'>Download Resume</a>
+        <a href={Hinal_Resume} download className='btn'>Download Resume</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
